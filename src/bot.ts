466b5a80fc9317ed3dd5ab424c20e8152a2966b4
maaaashi <doyaseizin.7@gmail.com>
2023-09-23 15:46:52 +0900

    fix parameter

--- src/bot.ts
+++ src/bot.ts
@@ -27,7 +27,7 @@ const handleTextMessage = async (event: MessageEvent): Promise<void> => {
     const response = await fetch(url, {
       method: 'POST',
       body: JSON.stringify({
-        userMessage,
+        message: userMessage,
       }),
     })
 
